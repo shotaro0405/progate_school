4dee0c7dc1cbcd1d08917a744e7e30f691347f7e
shotaro0405 <09hn054s@gmail.com>
2016-03-06 11:30:07 +0900

    carusel sakusei

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(function(){
+
+
+//もっと詳しく
+  $('#read-more').click(function(){
+    var $read = $('#more-text');
+    if($read.hasClass('open')){
+      $read.removeClass('open');
+      $read.fadeIn();
+      $('#read-more').html('閉じる');
+    }else{
+      $read.addClass('open');
+      $read.fadeOut();
+      $('#read-more').html('もっと詳しく');
+    }
+  });
+
+//学習する言語
+  $('.item').hover(function(){
+    $(this).find('p').fadeIn();
+  },function(){
+    $(this).find('p').fadeOut();
+  });
+
+
+//カルーセル
+$('.single-item').slick({
+  autoplay:true,
+  autoplaySpeed:1500,
+  dots:true,
+  pauseOnHover:true
+});
+
+
+});
+
+
+
