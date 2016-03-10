d04b3a99959f58277cf2ae31fb4cceb6bed2305a
shotaro0405 <09hn054s@gmail.com>
2016-03-10 21:35:10 +0900

    modal ver.01

--- js/script.js
+++ js/script.js
@@ -37,6 +37,35 @@ $('.article').hover(function(){
   $(this).find('img').removeClass('zoom-black');
 });
 
+//ヘッダーのスクロールナビ
+   // #で始まるアンカーをクリックした場合に処理
+   $('a[href^=#]').click(function() {
+      // スクロールの速度
+      var speed = 400; // ミリ秒
+      // アンカーの値取得
+      var href= $(this).attr("href");
+      // 移動先を取得
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      // 移動先を数値で取得
+      var position = target.offset().top;
+      // スムーススクロール
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
+
+//モーダル
+  $('#login-show').click(function() {
+    $('#login-modal').fadeIn();
+  });
+
+  $('.signup-show').click(function() {
+    $('#signup-modal').fadeIn();
+  });
+
+  $('.close-modal').click(function() {
+    $('#login-modal').fadeOut();
+    $('#signup-modal').fadeOut();
+  });
 
 });
 
